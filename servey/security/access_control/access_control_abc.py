50c79980882c2100cbcfd40fa4a37f7288b39027
Tim O'Farrell <tofarr@gmail.com>
2023-03-19 09:02:54 -0600

    Minor bug fix - make abstract method abstract

--- servey/security/access_control/access_control_abc.py
+++ servey/security/access_control/access_control_abc.py
@@ -1,9 +1,10 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Optional
 
 from servey.security.authorization import Authorization
 
 
 class AccessControlABC(ABC):
+    @abstractmethod
     def is_executable(self, authorization: Optional[Authorization]) -> bool:
         """Determine if this action_ may be executed using the authorization given"""
