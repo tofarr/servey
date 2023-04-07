c5222e06f45c3c9a78214ace3185b6bd82da46d7
Tim O'Farrell <tofarr@gmail.com>
2023-04-06 20:00:19 -0600

    Cloudfront API

--- examples/b_end_2_end/end_2_end/actions.py
+++ examples/b_end_2_end/end_2_end/actions.py
@@ -1,14 +1,7 @@
-from end_2_end.models import IndexModel
 from servey.action.action import action
-from servey.servey_web_page.web_page_trigger import WebPageTrigger
 from servey.trigger.web_trigger import WEB_GET
 
 
 @action(triggers=WEB_GET)
 def say_hello(name: str) -> str:
     return f"Hello {name}!"
-
-
-@action(triggers=WebPageTrigger(path="/"))
-def index() -> IndexModel:
-    return IndexModel()
