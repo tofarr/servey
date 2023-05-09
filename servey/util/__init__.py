d4cedd921ce7b95974d5025b35abd9979bf891dd
Tim O'Farrell <tofarr@gmail.com>
2023-05-08 22:11:53 -0600

    WIP

--- servey/util/__init__.py
+++ servey/util/__init__.py
@@ -38,6 +38,7 @@ def secure_hash(item: ExternalType) -> str:
 
 
 def secure_hash_content(content: bytes) -> str:
+    print(f"TRACE:secure_hash_content:{content.decode('UTF-8')}")
     sha = hashlib.sha256()
     sha.update(content)
     hash_bytes = sha.digest()
